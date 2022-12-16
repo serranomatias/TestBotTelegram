ff1758d899e1b5b8bd335e14058e08466f18de1b
Matiserrano99 <matiasserrano99@gmail.com>
2022-12-16 10:19:14 -0300

    Order

--- main.js
+++ main.js
@@ -5,11 +5,42 @@ const fs = require('fs');
 const axios = require('axios');
 
 
-const bot = new Telegraf(process.env.TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+
+bot.start((ctx)=> {
+    const userFirstName = ctx.update.message.from.first_name;
+    ctx.reply(`Hello ${userFirstName}`);
+    console.log(ctx);
+    console.log(ctx.update.message.from.first_name);
+});
+bot.help((ctx)=> {
+    ctx.reply("Ask for help to another person...");
+});
+
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+bot.hears('youtube', (ctx) => ctx.reply('Hey there'));
 
 function random(number) {
     return Math.floor(Math.random() * (number + 1));
 }
+
+
+bot.command('random', (ctx)=>{
+    ctx.reply(random(100) + ' tu numero');
+});
+
+bot.command('advancerandom', (ctx)=>{
+    const message = ctx.update.message.text;
+    const randomNumber = Number(message.split(' ')[1]);
+
+    if(isNaN(randomNumber) || randomNumber <= 0){
+        ctx.reply('Porfavor escribe un numero que sea valido');
+    } else {
+        ctx.reply(random(randomNumber) + '');
+    }
+})
+
 
 const downloadImage = (url, image_path, ctx) =>
     axios({ url, responseType: 'stream' }).then(
@@ -28,35 +59,22 @@ const downloadImage = (url, image_path, ctx) =>
         })
     );
 
-const helpMessage = 'Este es un bot de prueba para el tutorial de NewLabel sobre como crear un bot';
+    bot.on('photo', (ctx)=> {
+        const fileId = ctx.update.message.photo[1].file_id
+    
+        ctx.telegram.getFileLink(fileId).then((response)=>{
+            downloadImage(response.href, './photo.jpg', ctx)
+        })
+        ctx.reply('Me has enviado una foto');
+    })
+    
+    bot.command('sendphoto', (ctx)=> {
+        ctx.replyWithPhoto({source: 'photo.jpg'});
+    })
+    
+    
 
-bot.start((ctx)=> {
-    const userFirstName = ctx.update.message.from.first_name;
-    ctx.reply("Hola Lectores");
-    ctx.reply(`Hello ${userFirstName}`);
-    console.log(ctx);
-    console.log(ctx.update.message.from.first_name);
-});
-bot.help((ctx)=> {
-    ctx.reply(helpMessage);
-});
 
-bot.command('random', (ctx)=>{
-    console.log(random(100));
-    console.log(ctx);
-    ctx.reply(random(100) + ' tu numero');
-});
-
-bot.command('advancerandom', (ctx)=>{
-    const message = ctx.update.message.text;
-    const randomNumber = Number(message.split(' ')[1]);
-
-    if(isNaN(randomNumber) || randomNumber <= 0){
-        ctx.reply('Porfavor escribe un numero que sea valido');
-    } else {
-        ctx.reply(random(randomNumber) + '');
-    }
-})
 
 // bot.on(message('text'), (ctx)=> {
 //     const msg = ctx.update.message.text;
@@ -66,21 +84,7 @@ bot.command('advancerandom', (ctx)=>{
     
 // })
 
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-bot.hears('youtube', (ctx) => ctx.reply('Hey there'));
 
-bot.on('photo', (ctx)=> {
-    const fileId = ctx.update.message.photo[1].file_id
-
-    ctx.telegram.getFileLink(fileId).then((response)=>{
-        downloadImage(response.href, './photo.jpg', ctx)
-    })
-    ctx.reply('Me has enviado una foto');
-})
-
-bot.command('sendphoto', (ctx)=> {
-    ctx.replyWithPhoto({source: 'photo.jpg'});
-})
 
 
 bot.launch();
